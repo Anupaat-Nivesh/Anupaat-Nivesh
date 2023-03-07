b6c1f3a5bce9e71b8431b200fdcfe5a2a1e19e36
Gourav Chugh <gourav.chugh89@live.com>
2023-03-07 10:57:58 +0530

    updated About content

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 import './about.css';
 const About = () => {
     return (
-        <div>About</div>
+        <div>Started in 2018, Anupaat Nivesh is providing one2one consulting to start with your investment journey. Anupaat Nivesh aim to reach out to the common man and extend the opportunity to create wealth by providing them valuable and ethical financial advice. Equity investing has always been an grey area for most of people. However, historically there has been no better way to grow your money exponentially. We at Anupaat Nivesh help our customers with true and quality advise about investment options which can be huge wealth creator to middle class.
+
+        How we help our customers: 
+        - By Assessing individual's requirements and goals 
+        - By Creating awareness about financial freedom 
+        - By Providing coaching to avoid financial issues related to Investment needs and TAX benefits 
+        - By Providing platform for Investment in Equity MFs and our curated equity baskets
+        - By Managing their investments for higher returns
+        </div>
     )
 }
 
