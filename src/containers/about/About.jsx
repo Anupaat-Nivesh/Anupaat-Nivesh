3487812e4e6c12935e7d2463d435c5ee8cf8904c
Gourav Chugh <gourav.chugh89@live.com>
2023-03-13 16:58:51 +0530

    Updated OCntent

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import { ListGroupItem } from 'react-bootstrap';
 import './about.css';
 const About = () => {
     return (
         <div id='about' className='section__padding'>
-            <h1>About</h1>
-            <p> Started in 2018, Anupaat Nivesh is providing one2one consulting to start with your investment journey. Anupaat Nivesh aim to reach out to the common man and extend the opportunity to create wealth by providing them valuable and ethical financial advice. Equity investing has always been an grey area for most of people. However, historically there has been no better way to grow your money exponentially. We at Anupaat Nivesh help our customers with true and quality advise about investment options which can be huge wealth creator to middle class.
-
-                How we help our customers:
-                - By Assessing individual's requirements and goals
-                - By Creating awareness about financial freedom
-                - By Providing coaching to avoid financial issues related to Investment needs and TAX benefits
-                - By Providing platform for Investment in Equity MFs and our curated equity baskets
-                - By Managing their investments for higher returns</p>
+            <h1>About AnupaatNivesh</h1>
+            <p> Started in 2018, Anupaat Nivesh is providing one2one consulting to start with your investment journey. 
+                Anupaat Nivesh aim to reach out to the common man and extend the opportunity to create wealth by providing them valuable and ethical financial advice. 
+                Equity investing has always been an grey area for most of people. 
+                However, historically there has been no better way to grow your money exponentially. </p>      
+                <p>We at Anupaat Nivesh help our customers with true and quality advise about investment options which can be huge wealth creator to middle class.
+</p>
+                <p>How we help our customers:
+                    <ListGroupItem>
+                <li>By Assessing individual's requirements and goals</li>
+                <li>By Creating awareness about financial freedom</li>
+                <li>By Providing coaching to avoid financial issues related to Investment needs and TAX benefits</li>
+                <li>By Providing platform for Investment in Equity MFs and our curated equity baskets</li>By Providing coaching to avoid financial issues related to Investment needs and TAX benefits
+                <li>By Managing their investments for higher returns</li> 
+                </ListGroupItem>
+             </p>
         </div>
     )
 }
--- src/containers/whyanupaat/Whyanupaat.jsx
+++ src/containers/whyanupaat/Whyanupaat.jsx
@@ -11,13 +11,14 @@ const Whyanupaat = () => {
             <div className='anupaat__whyanupaat-main'>
 
                 <div className="anupaat__whyanupaat-heading">
-                    <h1> Why Anupaat Nivesh?</h1>
+                    <h1> Why Anupaat Nivesh? </h1>
 
                     <div className='anupaat_whyanupaat-heading-explanation'>
                         <div className='whyanupaat-subheading'>
                             <h2>Experience An Enhanced Online Investment Platform</h2>
                         </div>
-                        <p>Welcome to the town of investment! From Mutual Funds to Corporate Deposits,
+                        <p>Welcome to the town of investment! <p></p>From Mutual Funds to Corporate Deposits,
+                        
                             we hold a lot in store, to suit the goals and needs of every investor and every
                             risk appetite.
                         </p>
