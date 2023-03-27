c102016fccf21e70df9f4799fc25fb6e3ac8d689
Gourav Chugh <gourav.chugh89@live.com>
2023-03-27 12:48:54 +0530

    updated content formatting

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -23,8 +23,8 @@ const About = () => {
                 </div>
                 
                 <div className='aboutus-decription-content2'>
-                <p className='aboutus-description2'>Investing in the stock market is risky. But if managed and predicted correctly, it will have a high growth rate.
-                We at Anupaat Nivesh help our customers with true and quality advise about investment options which can be huge wealth creator to middle class.</p>
+                <div className='aboutus-description2'>Investing in the stock market is risky. But if managed and predicted correctly, it will have a high growth rate.</div>
+                <p>We at Anupaat Nivesh help our customers with true and quality advise about investment options which can be huge wealth creator to middle class.</p>
                 </div>
 
                 <div className='aboutus-description-points'>
