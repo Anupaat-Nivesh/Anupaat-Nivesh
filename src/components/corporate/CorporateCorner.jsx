97e1f7501f458d33f274dce98c5b1c7b88cb6321
Gourav Chugh <gourav.chugh89@live.com>
2023-05-05 09:20:05 +0530

    Corporate corner updated

--- src/components/corporate/CorporateCorner.jsx
+++ src/components/corporate/CorporateCorner.jsx
@@ -4,7 +4,28 @@ import './corporate.css';
 const CorporateCorner = () => {
     return (
         <div id='corporate'>CorporateCorner</div>
-    )
-}
+ )}
+       <div> Financial Wellness is the ideal employee benefit
+Upgrade your financial wellness programs with Banzai.
+   
 
+
+    Why Financial Literacy?
+Productivity
+Hands-on learning through simulated activities changes behavior. Banzai is for all ages and has limitless potential for implementation in schools, community groups, and employees at your organization.1
+
+Health
+We’ve always known that stress is bad for our health—and it’s never been truer than with financial stress. Nearly one-third of Americans say their lack of money has created obstacles to living a healthy lifestyle.2
+
+Attendance
+Tardiness and absenteeism tends to increase when employees struggle financially, which can also negatively affect their ability to perform well at work.3
+
+Well-Being
+Many people are struggling to save as a result of poor financial education. In fact, 37% of respondents in one large survey admitted to having no specific savings goal.4
+
+
+
+The Anupaat Nivesh Coach
+The Coach has no peer or precedent in financial education. It is a unique, virtual financial mentor that asks sophisticated questions and gives sophisticated answers. Your employees can create specific plans when they use the Coach to understand their own situation.
+</div>
 export default CorporateCorner
