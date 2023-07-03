7bf2132801431d62a96a3152aab984355f3437e5
LOKESH SINGH <lokesh1129ece@gmail.com>
2023-07-03 18:57:10 +0530

    changes the icon

--- src/containers/offerings/loanAgainstSecurities/LoanAgainstSecurities.jsx
+++ src/containers/offerings/loanAgainstSecurities/LoanAgainstSecurities.jsx
@@ -60,7 +60,7 @@ const LoanAgainstSecurities = () => {
                             <div className='card-img'> <img src={medicalIcon} alt="MedicalBillIcon" /></div>
                             <h3>Medical Expense</h3>
                             <p></p>
-                            <div className='card-img'> <img src={equity2} alt="image" /></div>
+                            
                         </div>
 
                     </Card>
@@ -70,8 +70,8 @@ const LoanAgainstSecurities = () => {
                             <div className='card-img'> <img src={travelIcon} alt="TravelBillIcon" /></div>
                             
                             <h3>Travel Expense</h3>
-                            <p>xyz</p></div>
-                        <div className='card-img'> <img src={equity2} alt="image" /></div>
+                            <p></p></div>
+                        
                     </Card>
                     <Card className="renovation-card" >
                         <div className="card-body">
@@ -79,8 +79,8 @@ const LoanAgainstSecurities = () => {
                            
                             <div className='card-img'> <img src={renovationIcon} alt="RenovationBillIcon" /></div>
                             <h3>Renovation Expense</h3>
-                            <p>xyz</p>
-                            <div className='card-img'> <img src={equity3} alt="image" /></div>
+                            <p></p>
+                            
                         </div>
                     </Card>
                     <Card className="education-card" >
@@ -88,8 +88,8 @@ const LoanAgainstSecurities = () => {
                             
                             <div className='card-img'> <img src={educationIcon} alt="EducationBillIcon" /></div>
                             <h3>Education Expense</h3>
-                            <p>xyz</p>
-                            <div className='card-img'> <img src={equity3} alt="image" /></div>
+                            <p></p>
+                            
                         </div>
                     </Card>
                 </div>
