2976b27a615e73ef296cdf761f84fd89c68a6383
gourav-chugh <gourav.chugh89@live.com>
2025-12-15 18:22:35 +0530

    Refactor: introduce MainLayout for shared UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
 import {
   Footer,
   About,
@@ -45,31 +46,32 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="home" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="whyanupaat" element={<Whyanupaat />} />
-        <Route path="offerings" element={<Offerings />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="ourApp" element={<OurApp />} />
+        <Route element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="whyanupaat" element={<Whyanupaat />} />
+          <Route path="offerings" element={<Offerings />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="ourApp" element={<OurApp />} />
 
-        <Route path="*" element={<NotFound />} />
-        {/* footer pages  */}
-        <Route path="faqs" element={<FAQs />} />
-        <Route path="partner-with-us" element={<PartnerWithUs />} />
-        <Route path="corporate" element={<CorporateCorner />} />
+          <Route path="*" element={<NotFound />} />
+          {/* footer pages  */}
+          <Route path="faqs" element={<FAQs />} />
+          <Route path="partner-with-us" element={<PartnerWithUs />} />
+          <Route path="corporate" element={<CorporateCorner />} />
 
-        <Route path="privacy-policy" element={<Privacypolicy />} />
+          <Route path="privacy-policy" element={<Privacypolicy />} />
 
-        {/* Offering pages  */}
-        <Route path="mutual-funds" element={<MutualFund />} />
-        <Route path="equity-basket" element={<EquityBasket />} />
-        <Route
-          path="loan-against-securities"
-          element={<LoanAgainstSecurities />}
-        />
-        <Route path="/tools" element={<Tools />} />
-
+          {/* Offering pages  */}
+          <Route path="mutual-funds" element={<MutualFund />} />
+          <Route path="equity-basket" element={<EquityBasket />} />
+          <Route
+            path="loan-against-securities"
+            element={<LoanAgainstSecurities />}
+          />
+          <Route path="/tools" element={<Tools />} />
+        </Route>
       </Routes>
 
       <Footer />
---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../containers/footer/Footer";
+import ScrollToTop from "../components/ScrollToTop";
+
+const MainLayout = () => {
+    return (
+        <>
+            <ScrollToTop />
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </>
+    );
+};
+
+export default MainLayout;
